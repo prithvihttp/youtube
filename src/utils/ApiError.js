671313fee4b4f441dd3f53fe4d700e53335392fc
prithvihttp <prithviraj6868@gmail.com>
2025-07-05 20:19:32 +0530

    first commit

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,23 +1,24 @@
 class ApiError extends Error {
     constructor(
         statusCode,
-        message = 'Something went wrong',
-        error = [],
+        message= "Something went wrong",
+        errors = [],
         stack = ""
-    ) {
-        super(message);
-        this.statusCode = statusCode;
-        this.data = null;
-        this.message = message;
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
         this.success = false;
-        this.errors = error;
+        this.errors = errors
 
         if (stack) {
-            this.stack = stack;
-        } else {
-            Error.captureStackTrace(this, this.constructor);
+            this.stack = stack
+        } else{
+            Error.captureStackTrace(this, this.constructor)
         }
+
     }
 }
 
-export { ApiError };
+export {ApiError}
